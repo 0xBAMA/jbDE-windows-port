925e7f15b567a672605ac35c178d3480ad3c8b90
0xBAMA <0xBAMA@users.noreply.github.com>
2025-03-04 03:52:40 -0600

    playing with SDFs

--- src/projects/PathTracing/Icarus/shaders/rayIntersect_SDF.cs.glsl
+++ src/projects/PathTracing/Icarus/shaders/rayIntersect_SDF.cs.glsl
@@ -347,17 +347,19 @@ float de ( vec3 p ) {
 
 	// return sceneDist;
 
-	const vec3 bboxSize = vec3( 2.0f );
+	const vec3 bboxSize = vec3( 100.0f );
 
 	{
-		const float scale = 2.0f;
+		const float scale = 20.0f;
 		const float d = max( fBox( p, bboxSize ), detrimental( p * scale ) / scale );
 		// const float d = deGAZ( p * scale ) / scale;
 		sceneDist = min( sceneDist, d );
 		if ( sceneDist == d && d < epsilon ) {
 			hitSurfaceType = ( NormalizedRandomFloat() < 0.1f ) ? DIFFUSE : MIRROR;
+			// hitSurfaceType = MIRROR;
 			// hitColor = checkerBoard( 10.0f, p ) ? vec3( nvidia ) : mix( vec3( 0.618f ), blood * 0.5f, vec3( 0.5f ) + ( curlNoise( p ).r + 1.0f ) * 0.1f );
-			hitColor = checkerBoard( 0.5f, p ) ? nvidia : carrot;
+			// hitColor = checkerBoard( 0.5f, p ) ? nvidia : carrot;
+			hitColor = nickel;
 	
 			// if ( mod( p.y, 0.75f ) < 0.1f ) {
 				// hitSurfaceType = EMISSIVE;
