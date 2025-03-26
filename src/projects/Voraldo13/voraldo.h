13fd2386533a381f4e3559a986d24908993b3bac
0xBAMA <0xBAMA@users.noreply.github.com>
2025-03-26 00:07:45 -0500

    remove redundant tonemap controls

--- src/projects/Voraldo13/voraldo.h
+++ src/projects/Voraldo13/voraldo.h
@@ -244,9 +244,6 @@ public:
 
 	void ImguiPass () {
 		ZoneScoped;
-		if ( tonemap.showTonemapWindow ) {
-			TonemapControlsWindow();
-		}
 
 		if ( showProfiler ) {
 			static ImGuiUtils::ProfilersWindow profilerWindow; // add new profiling data and render
