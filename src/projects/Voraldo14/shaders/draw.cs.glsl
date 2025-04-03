7dc5fbbec1f7135323b82518f9821006e105a767
0xBAMA <0xBAMA@users.noreply.github.com>
2025-04-03 05:07:44 -0500

    add Voraldo14

---
+++ src/projects/Voraldo14/shaders/draw.cs.glsl
@@ -0,0 +1,15 @@
+#version 430
+layout( local_size_x = 16, local_size_y = 16, local_size_z = 1 ) in;
+
+layout( binding = 0, rgba8ui ) uniform uimage2D blueNoiseTexture;
+layout( binding = 1, rgba16f ) uniform image2D accumulatorTexture;
+
+uniform float time;
+
+void main () {
+	// pixel location
+	ivec2 writeLoc = ivec2( gl_GlobalInvocationID.xy );
+
+	// write data to the image
+	imageStore( accumulatorTexture, writeLoc, vec4( vec3( 0.0f ), 1.0f ) );
+}
