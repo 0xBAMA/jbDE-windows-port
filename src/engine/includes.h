e237f103907f95aba087986e85f3f4fdd7fb2bc3
0xBAMA <0xBAMA@users.noreply.github.com>
2025-04-04 13:02:56 -0500

    add vec3 serdes for YAML lib, will need to do this for the rest of the GLM types

--- src/engine/includes.h
+++ src/engine/includes.h
@@ -211,6 +211,32 @@ using XMLDocument = tinyxml2::XMLDocument;
 // YAML lib
 #include "../utils/Serialization/yaml-cpp/include/yaml-cpp/yaml.h"
 
+namespace YAML {
+	// encode/decode for vector types (add more as we go)
+	template<>
+	struct convert<vec3> {
+		static Node encode( const vec3& rhs ) {
+			Node node;
+			node.push_back( rhs.x );
+			node.push_back( rhs.y );
+			node.push_back( rhs.z );
+			return node;
+		}
+
+		static bool decode( const Node& node, vec3& rhs ) {
+			if ( !node.IsSequence() || node.size() != 3 ) {
+				return false;
+			}
+
+			rhs.x = node[ 0 ].as<float>();
+			rhs.y = node[ 1 ].as<float>();
+			rhs.z = node[ 2 ].as<float>();
+			return true;
+		}
+	};
+}
+
+
 // .PLY file format I/O
 #include "../utils/happly/happly.h"
 
