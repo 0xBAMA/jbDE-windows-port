d9defaa39922bd614f9058911de7fb83f21425ac
jb <jb239812@ohio.edu>
2025-09-10 12:07:57 -0400

    colored voronoi

--- src/projects/PathTracing/Daedalus/shaders/pathtrace.cs.glsl
+++ src/projects/PathTracing/Daedalus/shaders/pathtrace.cs.glsl
@@ -1902,6 +1902,69 @@ float deLaceHall( vec3 p ){
 float deHalls(vec3 p){
 	return min(.65-length(fract(p+.5)-.5),p.y+0.05f);
 }
+// below from https://www.shadertoy.com/view/ltSfRW
+vec2 hash2( vec2 p )
+{
+	// texture based white noise
+	//return textureLod( iChannel0, (p+0.5)/256.0, 0.0 ).xy;
+
+	// procedural white noise
+	return fract(sin(vec2(dot(p,vec2(127.1,311.7)),dot(p,vec2(269.5,183.3))))*43758.5453);
+}
+
+// from https://www.shadertoy.com/view/ldl3W8 (bit modified to return desired values)
+// returns,
+//   x: distance to nearest neighbor
+//   y: cell id
+//   z: distance to border
+
+vec3 voronoi( in vec2 x )
+{
+	vec2 n = floor(x);
+	vec2 f = fract(x);
+
+	//----------------------------------
+	// first pass: regular voronoi
+	//----------------------------------
+	vec2 mg, mr;
+	float id;
+
+	float md = 8.0;
+	for( int j=-1; j<=1; j++ )
+	for( int i=-1; i<=1; i++ )
+	{
+		vec2 g = vec2(float(i),float(j));
+		vec2 o = hash2( n + g );
+		vec2 ao = o;
+		vec2 r = g + ao - f;
+		float d = dot(r,r);
+
+		if( d<md )
+		{
+			md = d;
+			mr = r;
+			mg = g;
+			id = o.x+o.y;
+		}
+	}
+
+	//----------------------------------
+	// second pass: distance to borders
+	//----------------------------------
+	md = 8.0;
+	for( int j=-2; j<=2; j++ )
+	for( int i=-2; i<=2; i++ )
+	{
+		vec2 g = mg + vec2(float(i),float(j));
+		vec2 o = hash2( n + g );
+		vec2 r = g + o - f;
+
+		if( dot(mr-r,mr-r)>0.00001 )
+		md = min( md, dot( 0.5*(mr+r), normalize(r-mr) ) );
+	}
+
+	return vec3( length(mr), id, md );
+}
 //=============================================================================================================================
 #include "oldTestChamber.h.glsl"
 #include "pbrConstants.glsl"
@@ -2063,7 +2126,10 @@ float de( in vec3 p ) {
 	 	sceneDist = min( sceneDist, d );
 	 	if ( sceneDist == d && d < epsilon ) {
 	 		hitSurfaceType = EMISSIVE_FRESNEL;
-	 		hitColor = vec3( 3.618f );
+//	 		hitColor = vec3( 3.618f * honey );
+
+			vec3 c = voronoi( pOriginal.xz * 5.0f );
+			hitColor = pow( saturate( smoothstep( c.z, 0.0f, 0.12f ) ), 0.25f ) * ( honey * 1.8f * saturate( 0.5f + 0.5f * cos( c.y * 6.2831f + vec3( 0.0f, 1.0f, 2.0f ) ) ) );
 	 	}
 	 }
 
