c493e004048c31c58ad1c1a2ea276c2758ac0ee2
jb <jb239812@ohio.edu>
2025-10-08 00:44:39 -0400

    setup to draw - maybe working, but renderdoc is not showing anything

--- src/projects/PathTracing/Newton/shaders/draw.cs.glsl
+++ src/projects/PathTracing/Newton/shaders/draw.cs.glsl
@@ -6,15 +6,21 @@ layout( binding = 0, rgba8ui ) uniform uimage2D blueNoiseTexture;
 layout( binding = 1, rgba16f ) uniform image2D accumulatorTexture;
 
 // film plane state - film plane is 3x as wide as it would be otherwise, to accomodate the separate channels
-layout( binding = 2, r32ui ) uniform uimage2D tallyImage;
+layout( binding = 3, r32ui ) uniform uimage2D filmPlaneImage;
+
+#include "colorspaceConversions.glsl"
 
 void main () {
 	// pixel location
 	ivec2 writeLoc = ivec2( gl_GlobalInvocationID.xy );
 
 	// grab the current state of the film plane... try to figure out how to map this to a color
-
+	uvec3 tallySample = uvec3(
+		imageLoad( filmPlaneImage, ivec2( 3, 1 ) * writeLoc + ivec2( 0, 0 ) ).r,
+		imageLoad( filmPlaneImage, ivec2( 3, 1 ) * writeLoc + ivec2( 1, 0 ) ).r,
+		imageLoad( filmPlaneImage, ivec2( 3, 1 ) * writeLoc + ivec2( 2, 0 ) ).r
+	);
 
 	// write the data to the accumulator, which will then be postprocessed and presented
-	imageStore( accumulatorTexture, writeLoc, vec4( 0.0f, 0.0f, 0.0f, 1.0f ) );
+	imageStore( accumulatorTexture, writeLoc, vec4( rgb_to_srgb( xyz_to_rgb( vec3( tallySample ) / 1000.0f ) ), 1.0f ) );
 }
