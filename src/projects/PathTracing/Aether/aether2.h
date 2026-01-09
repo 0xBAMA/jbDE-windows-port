a1bea975b029ed5974e4b390f85ae732446b3289
jb <jb239812@ohio.edu>
2026-01-09 04:48:45 -0500

    basic pushid/popid setup for lights

--- src/projects/PathTracing/Aether/aether2.h
+++ src/projects/PathTracing/Aether/aether2.h
@@ -517,73 +517,81 @@ public:
 		ImGui::Begin( "GelFilter" );
 
 		// we need to eventually show the importance sampling structure up top
+		for ( int l = 0; l < lightList.size(); l++ ) {
+			// disambiguating label hashes
+			ImGui::PushID( l );
 
-		// for now we're going to look at just the light config list (one light):
-		// curve texture preview
-		ImGui::Text( "" );
-		const int w = ImGui::GetContentRegionAvail().x;
-		ImGui::Image( ( ImTextureID ) ( void * ) intptr_t( textureManager->Get( "Filtered PDF Preview" ) ), ImVec2( w, w * ( 64.0f ) / ( 450.0f + 104 ) ) );
-		ImGui::Text( "" );
+			bool needsUpdate = false;
 
-		bool needsUpdate = false;
-		// ImGui::PushID(  );
+			// for now we're going to look at just the light config list (one light):
+			// curve texture preview
+			ImGui::Text( "" );
+			const int w = ImGui::GetContentRegionAvail().x;
+			ImGui::Image( ( ImTextureID ) ( void * ) intptr_t( textureManager->Get( "Filtered PDF Preview " + to_string( l ) ) ), ImVec2( w, w * ( 64.0f ) / ( 450.0f + 104 ) ) );
+			ImGui::Text( "" );
 
-		// source distribution picker
-		ImGui::Combo( ( string( "Light Type" ) ).c_str(), &lightList[ 0 ].sourcePDF, sourcePDFLabels, numSourcePDFs );
-		needsUpdate |= ImGui::IsItemEdited();
-
-		ImGui::SameLine();
-		if ( ImGui::Button( "Randomize" ) ) {
-			RandomizeLight( 0 );
-		}
-
-		// for gels
-		for ( int i = 0; i < lightList[ 0 ].gelStack.size(); i++ ) {
-			string iString = to_string( i );
-
-			ImGui::Separator();
-
-			// show gel picker
-			ImGui::Combo( ( "Gel##" + iString ).c_str(), &lightList[ 0 ].gelStack[ i ], gelFilterLabels, numGelFilters );
+			// source distribution picker
+			ImGui::Combo( ( string( "Light Type" ) ).c_str(), &lightList[ l ].sourcePDF, sourcePDFLabels, numSourcePDFs );
 			needsUpdate |= ImGui::IsItemEdited();
 
 			ImGui::SameLine();
-			if ( ImGui::Button( ( "Randomize##" + iString  ).c_str() ) ) {
-				rngi gelPick = rngi( 0, numGelFilters - 1 );
-				lightList[ 0 ].gelStack[ i ] = gelPick();
+			if ( ImGui::Button( "Randomize" ) ) {
+				RandomizeLight( l );
+			}
+
+			// for gels
+			for ( int i = 0; i < lightList[ l ].gelStack.size(); i++ ) {
+				string iString = to_string( i );
+
+				ImGui::Separator();
+
+				// show gel picker
+				ImGui::Combo( ( "Gel##" + iString ).c_str(), &lightList[ l ].gelStack[ i ], gelFilterLabels, numGelFilters );
+				needsUpdate |= ImGui::IsItemEdited();
+
+				ImGui::SameLine();
+				if ( ImGui::Button( ( "Randomize##" + iString  ).c_str() ) ) {
+					rngi gelPick = rngi( 0, numGelFilters - 1 );
+					lightList[ l ].gelStack[ i ] = gelPick();
+					needsUpdate = true;
+				}
+
+				ImGui::SameLine();
+				if ( ImGui::Button( ( "Remove##" + iString  ).c_str() ) ) {
+					lightList[ l ].gelStack.erase( lightList[ l ].gelStack.begin() + i );
+					needsUpdate = true;
+				}
+
+				// show selected gel preview color
+				vec3 col = gelPreviewColors[ lightList[ l ].gelStack[ i ] ];
+				vec4 sRGB = vec4( col[ 0 ], col[ 1 ], col[ 2 ], 255 );
+				bvec4 cutoff = lessThan( sRGB, vec4( 0.04045f ) );
+				vec4 higher = pow( ( sRGB + vec4( 0.055f ) ) / vec4( 1.055f ), vec4( 2.4f ) );
+				vec4 lower = sRGB / vec4( 12.92f );
+				vec4 r =  mix( higher, lower, cutoff );
+				if ( ImGui::ColorButton( ( "##ColorSquare" + iString ).c_str(), ImColor( r.r, r.g, r.b ), ImGuiColorEditFlags_NoAlpha, ImVec2(16, 16 ) ) ) {}
+				ImGui::SameLine();
+
+				// show selected gel description
+				ImGui::TextWrapped( "%s", gelFilterDescriptions[ lightList[ l ].gelStack[ i ] ] );
+			}
+
+			// button to add a new gel to the stack
+			if ( ImGui::Button( "Add Gel" ) ) {
+				lightList[ l ].gelStack.emplace_back();
 				needsUpdate = true;
 			}
 
-			ImGui::SameLine();
-			if ( ImGui::Button( ( "Remove##" + iString  ).c_str() ) ) {
-				lightList[ 0 ].gelStack.erase( lightList[ 0 ].gelStack.begin() + i );
-				needsUpdate = true;
+			if ( needsUpdate ) {
+				ComputeLightStack( l );
 			}
 
-			// show selected gel preview color
-			vec3 col = gelPreviewColors[ lightList[ 0 ].gelStack[ i ] ];
-			vec4 sRGB = vec4( col[ 0 ], col[ 1 ], col[ 2 ], 255 );
-			bvec4 cutoff = lessThan( sRGB, vec4( 0.04045f ) );
-			vec4 higher = pow( ( sRGB + vec4( 0.055f ) ) / vec4( 1.055f ), vec4( 2.4f ) );
-			vec4 lower = sRGB / vec4( 12.92f );
-			vec4 r =  mix( higher, lower, cutoff );
-			if ( ImGui::ColorButton( ( "##ColorSquare" + iString ).c_str(), ImColor( r.r, r.g, r.b ), ImGuiColorEditFlags_NoAlpha, ImVec2(16, 16 ) ) ) {}
-			ImGui::SameLine();
-
-			// show selected gel description
-			// ImGui::SameLine();
-			ImGui::TextWrapped( "%s", gelFilterDescriptions[ lightList[ 0 ].gelStack[ i ] ] );
-			// ImGui::PopID();
+			// unapply "i" pushID
+			ImGui::PopID();
 		}
 
-		// button to add a new gel to the stack
-		if ( ImGui::Button( "Add Gel" ) ) {
-			lightList[ 0 ].gelStack.emplace_back();
-			needsUpdate = true;
-		}
-
-		if ( needsUpdate ) {
-			ComputeLightStack( 0 );
+		if ( ImGui::Button( "Add Light" ) ) {
+			AddLight();
 		}
 
 		ImGui::End();
